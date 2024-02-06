9d08a93cf989b206ad8252124532dcd7cd7e66f5
aakashbishnoi1 <8xaakash@gmail.com>
2024-02-06 09:30:50 +0530

    render the scene and camera

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ import * as THREE from "three";
 // Setting up camera, scene and a renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
-const renderer = new THREE.WebGL1Renderer({canvas: document.getElementById("bg")})
-renderer.pixelRatio(window.devicePixelRatio);
+const renderer = new THREE.WebGL1Renderer({canvas: document.getElementById("bg")});
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.z = 30;
+camera.position.setZ(10);
+
+renderer.render(scene,camera);
