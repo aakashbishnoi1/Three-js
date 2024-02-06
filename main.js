a0d0d8afef9607b5ef284b499684704b9f4718e1
aakashbishnoi1 <8xaakash@gmail.com>
2024-02-06 10:02:36 +0530

    Add the model
    to the scene and animated it using the animate function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import './style.css'
+import "./style.css"
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // Setting up camera, scene and a renderer
 const scene = new THREE.Scene();
@@ -7,6 +8,21 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGL1Renderer({canvas: document.getElementById("bg")});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(10);
+camera.position.setZ(1);
 
 renderer.render(scene,camera);
+
+const loader = new GLTFLoader();
+loader.load("Flower.glb", function(gltf){
+    scene.add(gltf.scene);
+}, undefined, function(error){
+    console.error(error);
+});
+
+let ambient = new THREE.AmbientLight(0xffffff);
+scene.add(ambient);
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate()
